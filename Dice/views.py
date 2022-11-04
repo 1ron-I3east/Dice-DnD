48a953b1b9bbfd1009c4905b33321842f37dbe45
Lev <levpr2002@gmail.com>
2022-11-04 21:21:08 +0300

    dicednd

---
+++ Dice/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import*
+import pandas as pd
+# Create your views here.
+def home(request):
+    item = Student.objects.all().values()
+    df = pd.DataFrame(item)
+   
+    mydict = {
+        "df": df.to_html()
+    }
+    df.to_excel("output.xlsx")
+    return render(request, 'index.html', context=mydict)
+# Create your views here.
